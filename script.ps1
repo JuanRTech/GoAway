031de6bce714934d7fc656fb40858923240e246c
Clinux-h <70550374+Clinux-h@users.noreply.github.com>
2021-01-30 17:11:48 -0600

    Added a for loop

--- script.ps1
+++ script.ps1
@@ -1,6 +1,12 @@
-$UninstallPath = "F:\SysinternalsSuite"
+$UninstallPath = "C:\Users\funguy7777\Documents\SysinternalsSuite"
 
 # Looks for files in the stated file path, finds them if they have the key term uninstall and lists them
 Get-ChildItem -Path "$($UninstallPath)\*" -Include *uninstall* 
+
+#for each loop that takes each file found called "uninstall.exe" and runs it
+foreach ($uninstallexe in Get-ChildItem -Path "$($UninstallPath)\*" -Include *uninstall* )
+{
+    Start-Process -FilePath $uninstallexe
+}
 
 cmd /c pause
